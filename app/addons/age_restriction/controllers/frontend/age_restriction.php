bb05fb40f9265caa5cb35cef5fb70362bcb5f439
Danendz <danilapayne@gmail.com>
2023-05-29 11:53:30 +0500

    Добавил уведомление о некорректной дате

--- app/addons/age_restriction/controllers/frontend/age_restriction.php
+++ app/addons/age_restriction/controllers/frontend/age_restriction.php
@@ -14,6 +14,7 @@
  ****************************************************************************/
 
 use Tygh\Enum\Addons\AgeRestriction\AgeRestrictionTypes;
+use Tygh\Enum\NotificationSeverity;
 
 if (!defined('BOOTSTRAP')) {
     die('Access denied');
@@ -23,10 +24,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($mode === 'verify') {
         $is_legit = fn_get_cookie(AGE_RESTRICTION_IS_LEGIT);
         $is_cookie_set = $is_legit === AgeRestrictionTypes::ALLOWED || $is_legit === AgeRestrictionTypes::DENIED;
-        if ($_REQUEST['age'] && !$is_cookie_set) {
-            $is_age_valid = fn_age_restriction_compare($_REQUEST['age']);
-            fn_set_cookie(AGE_RESTRICTION_IS_LEGIT, $is_age_valid);
+
+        if (empty($_REQUEST['age']) || $is_cookie_set) {
+            return [CONTROLLER_STATUS_OK];
         }
+
+        $is_age_valid = fn_age_restriction_compare($_REQUEST['age']);
+
+        if ($is_age_valid === false) {
+            fn_set_notification(
+                NotificationSeverity::ERROR,
+                __('error'),
+                __('age_restriction_wrong_date')
+            );
+            return [CONTROLLER_STATUS_OK];
+        }
+
+        fn_set_cookie(AGE_RESTRICTION_IS_LEGIT, $is_age_valid);
 
         //Redirect to redirect_url if exists
         return [CONTROLLER_STATUS_REDIRECT, $_REQUEST['redirect_url'] ?? ''];
